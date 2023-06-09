6bdfa1dd7795b66dcc51e43c66e27de06368d5af
nirenrawal <nire0007@stud.kea.dk>
2023-06-09 23:54:12 +0200

    test

--- enroll/tests.py
+++ enroll/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.shortcuts import render
+from .forms import StudentRegistration
+from .models import User
 
-# Create your tests here.
+
+class HomeViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_home_view(self):
+        request = self.factory.get('/home')
+        response = render(request, 'enroll/home.html', {'form': StudentRegistration(), 'stud': User.objects.all()})
+
+        self.assertEqual(response.status_code, 200)
+        # self.assertTemplateUsed(response, 'enroll/home.html')
+        self.assertContains(response, '<form')
+        # self.assertContains(response, 'method="post"')
+
+
+
