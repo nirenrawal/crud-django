b258fbb72306962bbc0a885b22cea2a1684a0a47
nirenrawal <nire0007@stud.kea.dk>
2023-06-09 23:47:19 +0200

    eb env

--- djangoajaxcrud/settings.py
+++ djangoajaxcrud/settings.py
@@ -75,18 +75,18 @@ WSGI_APPLICATION = 'djangoajaxcrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'cruddb',
-        'PASSWORD': 'niren123',
-        'HOST': 'django-crudapp.cgripxykghnb.us-west-2.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #  'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'postgres',
+    #     'USER': 'cruddb',
+    #     'PASSWORD': 'niren123',
+    #     'HOST': 'django-crudapp.cgripxykghnb.us-west-2.rds.amazonaws.com',
+    #     'PORT': '5432',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
